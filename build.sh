1859b45e63fda17123866c3a9b8c3091d31cbb42
Evan Borgstrom <eborgstrom@nerdwallet.com>
2016-10-21 16:19:21 -0700

    Switch to a script

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eux
+
+SERIALIZATION_PKG=${SERIALIZATION_PKG:-marshmallow}
+TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST:-nope}
+
+pip install -e .
+pip install codecov pytest
+pip install ${SERIALIZATION_PKG}
+
+SERIALIZATION_PKG=${SERIALIZATION_PKG} coverage run --source=dynamorm setup.py test
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+    if [ -z "`git diff origin/master setup.py | grep '\+.*version='`" ]; then
+        printf "\n\n\n\nBump version in setup.py!\n\n\n\n"
+        exit 1
+    fi
+fi
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ ! -z "${GH_TOKEN}"]; then
+    pip install travis-sphinx
+    travis-sphinx --source=docs build
+    travis-sphinx deploy
+fi
