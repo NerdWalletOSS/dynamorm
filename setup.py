656303736ec79f27f186b2782e98ba8866ec4f2a
Evan Borgstrom <eborgstrom@nerdwallet.com>
2017-04-21 15:15:55 -0700

    Relax boto3 dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as readme_fd:
 
 setup(
     name='dynamorm',
-    version='0.0.10',
+    version='0.0.11',
     description='DynamORM is a Python object relation mapping library for Amazon\'s DynamoDB service.',
     long_description=long_description,
     author='Evan Borgstrom',
@@ -17,7 +17,7 @@ setup(
         'pytest-runner',
     ],
     install_requires=[
-        'boto3>=1.3,<1.4',
+        'boto3>=1.3,<2.0',
         'pytest>=2.9,<3.0',
         'six',
     ],
