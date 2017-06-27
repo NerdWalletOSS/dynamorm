4538f0ecc1d30e2181bbdcde11f61d3506e88e37
Evan Borgstrom <eborgstrom@nerdwallet.com>
2017-06-27 15:22:26 -0700

    Set Marshamllow testmodel field type

--- tests/conftest.py
+++ tests/conftest.py
@@ -36,7 +36,7 @@ def TestModel():
                 baz = fields.String(required=True)
                 count = fields.Integer()
                 child = fields.Dict()
-                things = TODO
+                things = fields.List(fields.String())
 
             def business_logic(self):
                 return 'http://art.lawver.net/funny/internet.jpg?foo={foo}&bar={bar}'.format(
