a009e841d1ea62853b53563754ccc121ae47bd39
Evan Borgstrom <eborgstrom@nerdwallet.com>
2017-07-29 15:24:51 -0700

    Update documentation in base type

--- dynamorm/types/base.py
+++ dynamorm/types/base.py
@@ -2,7 +2,7 @@ class BaseModel(object):
     """``BaseModel`` is the base class for ``types.Model``. It must define ``dynamallow_validate`` which runs validation
     in your desired serialization library, ``dynamallow_fields`` which returns a dictionary of key value pairs
     where keys are attributes and values are the type of the attribute, and ``field_to_dynamo_type`` which returns
-    the dynamo type character for the input type (we implement schematics_ and marshmallow_).
+    the dynamo type character for the input type (we currently implement schematics_ and marshmallow_).
 
     .. _schematics: https://schematics.readthedocs.io/en/latest/
     .. _marshmallow: https://marshmallow.readthedocs.io/en/latest/
@@ -18,12 +18,17 @@ class BaseModel(object):
         raise NotImplementedError('{0} class must implement dynamallow_fields'.format(cls.__name__))
 
     @classmethod
-    def dynamallow_validate(cls, obj, partial=False):
+    def dynamallow_validate(cls, obj, partial=False, native=False):
         """
         Given a dictionary representing a blob from dynamo, this method will validate the blob
         given the desired validation library.
 
-        Returns a dictionary representing the sanitized input ``obj``. On validation failure,
-        this should raise ``dynamallow.exc.ValidationError``.
+        If partial is true then the underlying validation library should allow for partial objects.
+
+        If native is true then the underlying validation library should return a dictionary of native python values
+        (i.e. datetime.datetime), otherwise it should return a dictionary of primitive values (i.e. a string
+        representation of a date time value).
+
+        On validation failure, this should raise ``dynamallow.exc.ValidationError``.
         """
         raise NotImplementedError('{0} class must implement dynamallow_validate'.format(cls.__name__))
