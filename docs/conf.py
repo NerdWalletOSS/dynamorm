b03a874349311a3d9c2912aba8d2342605ea2f0a
Evan Borgstrom <eborgstrom@nerdwallet.com>
2017-10-16 15:06:27 -0700

    Lower case repo in doc config so buttons work

--- docs/conf.py
+++ docs/conf.py
@@ -145,7 +145,7 @@ html_theme = 'alabaster'
 html_theme_options = {
     'description': u'Python object relation mapping library for Amazon\'s DynamoDB service.<br />',
     'github_user': 'NerdWalletOSS',
-    'github_repo': 'DynamORM',
+    'github_repo': 'dynamorm',
     'github_banner': True,
     'github_type': 'star',
     'travis_button': True,
