ad36e6a2e115bd4de3ba757d9f3b41fb9232fb54
Evan Borgstrom <eborgstrom@nerdwallet.com>
2019-09-28 10:23:17 +0800

    Refactor tests & docs to make contribution easier

--- docs/developing.rst
+++ docs/developing.rst
@@ -1,13 +1,12 @@
 Developing
 ==========
 
-To create a virtualenv and pull in the required dependencies:
+To create a virtualenv and activate it.
 
 .. code-block:: bash
-    
-    virtualenv ~/.virtualenvs/dynamallow
-    source ~/.virtualenvs/dynamallow/bin/activate
-    pip install -e .
+
+    virtualenv ~/.virtualenvs/dynamorm
+    source ~/.virtualenvs/dynamorm/bin/activate
 
 
 Running the tests
@@ -17,7 +16,10 @@ From the activated virtualenv:
 
 .. code-block:: bash
 
-    python setup.py test
+    ./test.sh
 
-The tests will pull down the latest copy of DynamoDB Local from S3 and place it in ``build/dynamo-local``.  A copy will
-be started on a random high port, running with ``-inMemory``, at the start of the test run and shutdown after the run.
+The tests will pull down the latest copy of DynamoDB Local from S3 and place it in ``build/dynamo-local``.  A copy will be started on a random high port, running with ``-inMemory``, at the start of the test run and shutdown after the run.
+
+By default the tests will run against both marshmallow and schematics.  You can manually set the ``SERIALIZATION_PKG`` environment variable to a desired package, and optional version, if you want to run against just one of them.
+
+See the source for ``test.sh`` for full details.
---
+++ test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# If we're not on the master branch
+if [ -z "${FORCE_TEST:-}" ] && [ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]; then
+    # Make sure the version has been bumped and a changelog entry has been added
+    if [ -z "$(git diff origin/master setup.py | grep '\+.*version=')" ]; then
+        printf "\n\n\n\nBump the version in setup.py!\n\n\n\n"
+        exit 1
+    elif [ -z "$(git diff origin/master CHANGELOG.rst)" ]; then
+        printf "\n\n\n\nAdd an entry to CHANGELOG.rst!\n\n\n\n"
+        exit 1
+    fi
+fi
+
+set -eux
+
+pip install -r requirements.txt
+pip install codecov pytest pytest-mock
+
+runtests() {
+    coverage run --source=dynamorm $(which py.test) -v -W ignore::schematics.deprecated.SchematicsDeprecationWarning tests/
+}
+
+if [ ! -z "${SERIALIZATION_PKG:-}" ]; then
+    pip install "${SERIALIZATION_PKG}"
+    runtests
+else
+    for pkg in marshmallow schematics; do
+        export SERIALIZATION_PKG="${pkg}"
+	runtests
+    done
+fi
+
+
+# only build docs on py3.7
+if [ "${TRAVIS_PYTHON_VERSION:-}" = "3.7" ]; then
+    pip install travis-sphinx
+    travis-sphinx build --source docs
+
+    # push if we have a token and this isn't a pr build
+    if [ "${TRAVIS_PULL_REQUEST:-}" = "false" ] && [ ! -z "${GH_TOKEN:-}" ]; then
+        travis-sphinx deploy
+    fi
+fi
